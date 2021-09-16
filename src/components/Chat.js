d5dfe999117212446fc96092677941af12a6973b
Rezuan Khamukov <Khamukov19@gmail.com>
2021-09-17 00:16:58 +0300

    добавлено поле для ввода сообшения и кнопка отправки данных на firebase

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Context } from '../index'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { Container, Grid, Button } from '@material-ui/core'
+import TextField from "@material-ui/core/TextField"
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+import Loader from './Loader'
+import firebase from 'firebase'
+
 
 const Chat = () => {
+    const {auth, firestore} = useContext(Context)
+    const [user] = useAuthState(auth)
+    const [value, setValue] = useState('')
+    const [messages, loading] = useCollectionData(
+        firestore.collection('messages').orderBy('createdAt')
+    )
+
+    const sendMessage = async () => {
+        firestore.collection('messages').add({
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            text: value,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setValue('')
+    }
+
+    if (loading) {
+        return <Loader/>
+    }
+
     return (
-        <div>
-            Chat
-        </div>
+        <Container>
+            <Grid container
+                justify={"center"}
+                style={{ height: window.innerHeight - 50, marginTop: 20 }}>
+                <div style={{ width: '80%', height: '68vh', border: '1px solid gray', overflow: 'auto' }}>
+
+                </div>
+                <Grid
+                    container
+                    direction={"column"}
+                    alignItems={"flex-end"}
+                    style={{ width: '80%' }}
+                >
+                    <TextField 
+                        fullWidth
+                        rowsMax={2}
+                        variant={"outlined"}
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    >
+                            
+                    </TextField>
+                    <Button 
+                        variant={"outlined"}
+                        style={{marginTop: "10px"}}
+                        onClick={sendMessage}
+                    >
+                        Отправить
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {AppBar, Toolbar, Grid, Button} from '@material-ui/core'
 import { LOGIN_ROUTE } from '../utils/consts'
 import { NavLink } from 'react-router-dom'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {Context} from '../index'
 
 export const Navbar = () => {
-    const user = false
+    const {auth} = useContext(Context)
+    const [user] = useAuthState(auth)
+
     return (
         <AppBar position="static" color={"secondary"}>
             <Toolbar variant={"dense"}>
                 <Grid container justify={"flex-end"}>
                     {user ?
-                        <Button variant={"outlined"}>Выйти</Button>
+                        <Button onClick={() => auth.signOut()} variant={"outlined"}>Выйти</Button>
                     :
                     <NavLink to={LOGIN_ROUTE}>
                         <Button variant={"outlined"}>Логин</Button>
